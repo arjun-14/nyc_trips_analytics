eacb3ac17f47b40aa7b81e4eeddf5c9163802ce8
Arjun Narayanan <arjun14081999@gmail.com>
2025-06-27 11:17:26 -0500

    Create create_for_hire_vehicle_trips.sql

---
+++ scripts/bronze/create_for_hire_vehicle_trips.sql
@@ -0,0 +1,18 @@
+/*
+-- Script: create_for_hire_vehicle_trips.sql
+-- Dataset: bronze
+-- Table: for_hire_vehicle_trips
+-- Source URL: https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page
+-- Description: This table stores raw trip-level data for traditional for-hire vehicles in NYC for the year 2024.
+*/
+
+CREATE OR REPLACE TABLE `nyc-trips-analytics.bronze.for_hire_vehicle_trips`
+(
+  dispatching_base_num STRING,
+  pickup_datetime TIMESTAMP,
+  dropoff_datetime TIMESTAMP,
+  pulocation_id	INT64,
+  dolocation_id	INT64,
+  sr_flag	INT64,
+  affiliated_base_number STRING
+);
