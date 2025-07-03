37d0610a09d4aa832af73e82d7b4965b92bb1e7e
Arjun Narayanan <arjun14081999@gmail.com>
2025-07-03 11:59:09 -0500

    Create create_taxi_zone_lookup.sql

---
+++ scripts/bronze/create_taxi_zone_lookup.sql
@@ -0,0 +1,15 @@
+/*
+-- Script: create_taxi_zone_lookup.sql
+-- Dataset: bronze
+-- Table: taxi_zone_lookup
+-- Source URL: https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page
+-- Description: This table stores reference data mapping taxi zone Location IDs to boroughs, zones, and service zones in NYC.
+*/
+
+CREATE OR REPLACE TABLE `nyc-trips-analytics.bronze.taxi_zone_lookup`
+(
+  location_id INT64,
+  borough STRING,
+  zone STRING,
+  service_zone STRING
+);
