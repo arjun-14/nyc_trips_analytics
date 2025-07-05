36ee1f4eb32bd0cc0925e3785c1076369fc0012f
Arjun Adukathil <arjun.adukathil@tamu.edu>
2025-07-05 18:25:56 -0500

    Create create_silver_high_volume_for_hire_vehicle_trips.sql

---
+++ scripts/silver/tables/create_silver_high_volume_for_hire_vehicle_trips.sql
@@ -0,0 +1,121 @@
+/*
+-- Script: create_silver_high_volume_for_hire_vehicle_trips.sql
+-- Dataset: silver
+-- Table: high_volume_for_hire_vehicle_trips
+-- Source Table: nyc-trips-analytics.bronze.high_volume_for_hire_vehicle_trips
+-- Description: This table stores cleansed and enriched trip-level data for high volume for-hire vehicles 
+--              (e.g., Uber, Lyft) in NYC for the year 2024.
+--              Includes derived fields such as total amount, fare per mile, trip duration, time-of-day 
+--              classification, tip percentage and data quality flags for fare amount and trip duration.          
+-- Version: 1.0 
+-- Last Modified: 2025-07-04
+*/
+
+CREATE OR REPLACE TABLE `nyc-trips-analytics.silver.high_volume_for_hire_vehicle_trips` AS
+WITH high_volume_for_hire_vehicle AS(
+  SELECT
+  TO_HEX(SHA256(CONCAT(
+      CAST(request_datetime AS STRING), '_',
+      CAST(pickup_datetime AS STRING), '_',
+      CAST(dropoff_datetime AS STRING), '_',
+      CAST(pulocation_id AS STRING), '_',
+      CAST(dolocation_id AS STRING)
+    ))) AS trip_id,
+    hvfhs_license_num,
+    dispatching_base_num,
+    originating_base_num,
+    request_datetime,
+    on_scene_datetime,
+    pickup_datetime,
+    dropoff_datetime,
+    pulocation_id,
+    dolocation_id,
+    trip_miles,
+    trip_time,
+    base_passenger_fare,
+    tolls,
+    bcf,
+    sales_tax,
+    congestion_surcharge,
+    airport_fee,
+    tips,
+    driver_pay,
+    shared_request_flag,
+    shared_match_flag,
+    access_a_ride_flag,
+    wav_request_flag,
+    wav_match_flag,
+
+    --derived columns
+    base_passenger_fare + tolls + bcf + sales_tax + congestion_surcharge + airport_fee AS total_amount,
+    CASE 
+      WHEN trip_miles > 0 THEN ROUND(base_passenger_fare / trip_miles, 2)
+      ELSE NULL
+    END AS fare_per_mile,
+    TIMESTAMP_DIFF(dropoff_datetime, pickup_datetime, SECOND)  AS trip_duration_seconds, 
+    CASE 
+      WHEN EXTRACT(HOUR FROM pickup_datetime) BETWEEN 6 AND 18 THEN 'Day'
+      ELSE 'Night'
+    END AS time_of_day_bucket,
+    FORMAT_TIMESTAMP('%A', pickup_datetime) AS pickup_day_of_week,
+    CASE 
+      WHEN base_passenger_fare > 0 THEN ROUND(tips / base_passenger_fare * 100, 2)
+      ELSE NULL
+    END AS tip_percentage,
+
+    -- data quality flags
+    CASE
+      WHEN base_passenger_fare < 0 THEN 'INVALID'
+      ELSE 'VALID'
+    END AS base_passenger_fare_flag,
+    CASE
+      WHEN TIMESTAMP_DIFF(dropoff_datetime, pickup_datetime, SECOND) < 0 THEN 'INVALID'
+      ELSE 'VALID'
+    END AS trip_duration_seconds_flag,
+    
+    -- housekeeping columns
+    CURRENT_TIMESTAMP() AS insert_timestamp,
+    CURRENT_TIMESTAMP() AS update_timestamp
+
+  FROM
+    `nyc-trips-analytics.bronze.high_volume_for_hire_vehicle_trips`
+)
+SELECT
+  trip_id,
+  hvfhs_license_num,
+  dispatching_base_num,
+  originating_base_num,
+  request_datetime,
+  on_scene_datetime,
+  pickup_datetime,
+  dropoff_datetime,
+  pulocation_id,
+  dolocation_id,
+  trip_miles,
+  trip_time,
+  base_passenger_fare,
+  tolls,
+  bcf,
+  sales_tax,
+  congestion_surcharge,
+  airport_fee,
+  tips,
+  driver_pay,
+  shared_request_flag,
+  shared_match_flag,
+  access_a_ride_flag,
+  wav_request_flag,
+  wav_match_flag,
+  total_amount,
+  fare_per_mile,
+  trip_duration_seconds,
+  time_of_day_bucket,
+  pickup_day_of_week,
+  tip_percentage,
+  base_passenger_fare_flag,
+  trip_duration_seconds_flag,
+  insert_timestamp,
+  update_timestamp
+FROM
+  high_volume_for_hire_vehicle
+QUALIFY ROW_NUMBER() OVER(PARTITION BY trip_id) = 1
